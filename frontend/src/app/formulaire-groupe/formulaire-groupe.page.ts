6a2a7e7f9574e28a7ebac581922d3d8eb4b0e527
Awa KEITA <awakeita627@gmail.com>
2021-12-21 14:45:48 +0000

    frontend

---
+++ frontend/src/app/formulaire-groupe/formulaire-groupe.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-groupe',
+  templateUrl: './formulaire-groupe.page.html',
+  styleUrls: ['./formulaire-groupe.page.scss'],
+})
+export class FormulaireGroupePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
